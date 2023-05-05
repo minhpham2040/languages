3ded4f695a683083b10c61ca3edc1d0e75d9d54f
minhpham2040 <119834296+minhpham2040@users.noreply.github.com>
2023-05-05 15:08:17 +0700

    Create detail.php

---
+++ php/basic/detail.php
@@ -0,0 +1,34 @@
+<?php
+  include_one("connections/connection.php");
+
+  $con = connection();
+
+  if(isset($_GET['id'])){
+    
+        $id = $_GET['id'];
+
+        $sql = "SELECT * FROM products WHERE id = '$id'";
+        $row = $con->query($sql) or die($con->error);
+  }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detail Product</title>
+</head>
+<body>
+  <div>
+    <div>
+        <a href="./edit.php">Edit</a>
+        <a href="./delete.php">Delete</a>
+    </div>
+    <div class="product">
+        <h4 class="name"><?php echo($row['name']);?></h4>
+        <span class="price"><?php echo($row['price'])?></span>
+    </div>
+  </div>
+</body>
+</html>  
