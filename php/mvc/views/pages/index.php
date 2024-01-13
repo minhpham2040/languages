12441e025a81857a07d40ec1ace2a043532d85d8
minhpham2040 <119834296+minhpham2040@users.noreply.github.com>
2024-01-13 12:22:31 +0700

    Create index.php

---
+++ php/mvc/views/pages/index.php
@@ -0,0 +1,5 @@
+<div class="content1">
+    <?php require_once"./mvc/views/partials/home/cars.php"; ?>
+    <?php require_once"./mvc/views/partials/home/fruits.php"; ?>
+    <?php require_once"./mvc/views/partials/home/moto.php"; ?>
+</div>
