3aead0138bcf4d9ec430d8127a152f7ad8e01c9e
minhpham2040 <119834296+minhpham2040@users.noreply.github.com>
2024-03-14 13:03:27 +0700

    Create config.js

---
+++ nodejs/basic/config.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+// Replace the following with your Atlas connection string
+const url =
+  "mongodb+srv://minhpham:123@cluster0.jqvhm.mongodb.net/tutorials?retryWrites=true&w=majority";
+//const client = new MongoClient(url);
+
+// The database to use.
+//const dbName = "tutorials";
+
+async function connect() {
+  try {
+    await mongoose.connect(url);
+    console.log("Connected correctly to server");
+    //const db = client.db(dbName);
+
+    // Use the collection "people"
+    //const col = db.collection("courses");
+
+    // Construct a document
+    //  let personDocument = {
+    //      "name": { "first": "Alan", "last": "Turing" },
+    //      "birth": new Date(1912, 5, 23), // June 23, 1912
+    //      "death": new Date(1954, 5, 7),  // June 7, 1954
+    //      "contribs": [ "Turing machine", "Turing test", "Turingery" ],
+    //      "views": 1250000
+    //  }
+
+    // Insert a single document, wait for promise so we can read it back
+    //const p = await col.insertOne(personDocument);
+    // Find one document
+    //const myDoc = await col.findOne();
+    // Print to the console
+    //console.log(myDoc);
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    //await client.close();
+  }
+}
+
+module.exports = { connect };
