508ee4ebbbce2118ad297aa1eff308d2c55aeec2
minhpham2040 <119834296+minhpham2040@users.noreply.github.com>
2024-03-14 13:07:45 +0700

    Create index.js

---
+++ nodejs/basic/resources/routes/index.js
@@ -0,0 +1,12 @@
+const newsRouter = require("./news");
+const siteRouter = require("./site");
+const courseRouter = require("./course");
+const meRouter = require("./me");
+function route(app) {
+  app.use("/me", meRouter);
+  app.use("/courses", courseRouter);
+  app.use("/news", newsRouter);
+
+  app.use("/", siteRouter);
+}
+module.exports = route;
